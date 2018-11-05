openucx/ucx

UCT/IB/MLX5: Upstream compact AV support

src/uct/ib/mlx5/ib_mlx5_dv.c
@@ -89,6 +89,10 @@ static ucs_status_t uct_ib_mlx5_device_init(uct_ib_device_t *dev)
         if (UCT_GET(query_hca_cap_out, out, capability.cmd_hca_cap.dct)) {
             dev->flags |= UCT_IB_DEVICE_FLAG_DC;
         }
+        if (UCT_GET(query_hca_cap_out, out,
+                    capability.cmd_hca_cap.compact_address_vector)) {
+            dev->flags |= UCT_IB_DEVICE_FLAG_AV;
+        }
     }
 #endif
     if (ret != 0) {
@@ -100,6 +104,15 @@ static ucs_status_t uct_ib_mlx5_device_init(uct_ib_device_t *dev)
 
 UCT_IB_DEVICE_INIT(uct_ib_mlx5_device_init);
 
+#endif
+
+#if HAVE_DECL_MLX5DV_DEVX_GENERAL_CMD
+ucs_status_t uct_ib_mlx5_get_compact_av(uct_ib_iface_t *iface, int *compact_av)
+{
+    *compact_av = !!(uct_ib_iface_device(iface)->flags & UCT_IB_DEVICE_FLAG_AV);
+
+    return UCS_OK;
+}
 #endif
 
 int uct_ib_mlx5dv_arm_cq(uct_ib_mlx5_cq_t *cq, int solicited)

openeuler-mirror/kucx

src/uct/ib/mlx5/ib_mlx5_dv.c
@@ -91,6 +91,10 @@ static ucs_status_t uct_ib_mlx5dv_device_init(uct_ib_device_t *dev)
         if (!UCT_IB_MLX5DV_GET(query_hca_cap_out, out, capability.cmd_hca_cap.dct)) {
             no_dc = 1;
         }
+        if (UCT_IB_MLX5DV_GET(query_hca_cap_out, out,
+                    capability.cmd_hca_cap.compact_address_vector)) {
+            dev->flags |= UCT_IB_DEVICE_FLAG_AV;
+        }
     } else if ((errno != EPERM) &&
                (errno != EPROTONOSUPPORT) &&
                (errno != EOPNOTSUPP)) {
@@ -106,6 +110,15 @@ static ucs_status_t uct_ib_mlx5dv_device_init(uct_ib_device_t *dev)
 }
 
 UCT_IB_DEVICE_INIT(uct_ib_mlx5dv_device_init);
+
+#if HAVE_DECL_MLX5DV_DEVX_GENERAL_CMD
+ucs_status_t uct_ib_mlx5_get_compact_av(uct_ib_iface_t *iface, int *compact_av)
+{
+    *compact_av = !!(uct_ib_iface_device(iface)->flags & UCT_IB_DEVICE_FLAG_AV);
+
+    return UCS_OK;
+}
+#endif
 
 int uct_ib_mlx5dv_arm_cq(uct_ib_mlx5_cq_t *cq, int solicited)
 {