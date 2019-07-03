openucx/ucx

UCT/IB/MLX5: Move res_domain to uct_ib_mlx5_qp_t

src/uct/ib/rc/accel/rc_mlx5_ep.c
@@ -902,6 +902,7 @@ static UCS_CLASS_CLEANUP_FUNC(uct_rc_mlx5_ep_t)
 #if IBV_HW_TM
     if (UCT_RC_MLX5_TM_ENABLED(iface)) {
         uct_rc_iface_remove_qp(&iface->super, self->tm_qp.verbs.qp->qp_num);
+        uct_ib_mlx5_iface_cleanup_res_domain(&self->tm_qp);
         uct_rc_mlx5_ep_clean_qp(self, self->tm_qp.verbs.qp);
     }
 #endif

openeuler-mirror/kucx

src/uct/ib/rc/accel/rc_mlx5_ep.c
@@ -920,6 +920,7 @@ static UCS_CLASS_CLEANUP_FUNC(uct_rc_mlx5_ep_t)
 #if IBV_HW_TM
     if (UCT_RC_MLX5_TM_ENABLED(iface)) {
         uct_rc_mlx5_ep_clean_qp(self, self->tm_qp.verbs.qp);
+        uct_ib_mlx5_iface_put_res_domain(&self->tm_qp);
         uct_rc_iface_remove_qp(&iface->super, self->tm_qp.qp_num);
         uct_ib_destroy_qp(self->tm_qp.verbs.qp);
     }