petro-rudenko/ucx

JUCX: context.RegisterMemory - expose register by address + odp flags + allocation.

bindings/java/src/main/java/org/openucx/jucx/examples/UcxBenchmark.java
@@ -5,10 +5,7 @@
 
 package org.openucx.jucx.examples;
 
-import org.openucx.jucx.ucp.UcpContext;
-import org.openucx.jucx.ucp.UcpParams;
-import org.openucx.jucx.ucp.UcpWorker;
-import org.openucx.jucx.ucp.UcpWorkerParams;
+import org.openucx.jucx.ucp.*;
 
 import java.io.Closeable;
 import java.io.IOException;
@@ -32,7 +29,9 @@ public abstract class UcxBenchmark {
 
     protected static int numIterations;
 
-    protected static int totalSize;
+    protected static long totalSize;
+
+    protected static UcpRegisterMemoryParams allocationParams;
 
     private static String DESCRIPTION = "JUCX benchmark.\n" +
         "Run: \n" +
@@ -45,12 +44,14 @@ public abstract class UcxBenchmark {
         "s - IP address to bind sender listener (default: 0.0.0.0)\n" +
         "p - port to bind sender listener (default: 54321)\n" +
         "t - total size in bytes to transfer from sender to receiver (default 10000)\n" +
+        "o - on demand registration (default: false) \n" +
         "n - number of iterations (default 5)\n";
 
     static {
         argsMap.put("s", "0.0.0.0");
         argsMap.put("p", "54321");
         argsMap.put("t", "10000");
+        argsMap.put("o", "false");
         argsMap.put("n", "5");
     }
 
@@ -69,7 +70,11 @@ public abstract class UcxBenchmark {
         try {
             serverPort = Integer.parseInt(argsMap.get("p"));
             numIterations = Integer.parseInt(argsMap.get("n"));
-            totalSize = Integer.parseInt(argsMap.get("t"));
+            totalSize = Long.parseLong(argsMap.get("t"));
+            allocationParams = new UcpRegisterMemoryParams().allocate().setLength(totalSize);
+            if (argsMap.get("o").compareToIgnoreCase("true") == 0) {
+                allocationParams.nonBlocking();
+            }
         } catch (NumberFormatException ex) {
             System.out.println(DESCRIPTION);
             return false;

bindings/java/src/main/java/org/openucx/jucx/examples/UcxReadBWBenchmarkReceiver.java
@@ -7,6 +7,7 @@ package org.openucx.jucx.examples;
 
 import org.openucx.jucx.UcxCallback;
 import org.openucx.jucx.UcxRequest;
+import org.openucx.jucx.UcxUtils;
 import org.openucx.jucx.ucp.*;
 
 
@@ -16,7 +17,7 @@ import java.nio.ByteBuffer;
 
 public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         if (!initializeArguments(args)) {
             return;
         }
@@ -39,7 +40,7 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
         }
 
         long remoteAddress = recvBuffer.getLong();
-        long remoteSize = recvBuffer.getInt();
+        long remoteSize = recvBuffer.getLong();
         int remoteKeySize = recvBuffer.getInt();
         int rkeyBufferOffset = recvBuffer.position();
 
@@ -49,7 +50,7 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
         copyBuffer(recvBuffer, workerAddress, workerAdressSize);
 
         int remoteHashCode = recvBuffer.getInt();
-        System.out.printf("Received connection. Will read %d bytes from remote address %d\n",
+        System.out.printf("Received connection. Will read %d bytes from remote address %d%n",
             remoteSize, remoteAddress);
 
         UcpEndpoint endpoint = worker.newEndpoint(
@@ -60,10 +61,14 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
         UcpRemoteKey remoteKey = endpoint.unpackRemoteKey(recvBuffer);
         resources.push(remoteKey);
 
-        ByteBuffer data = ByteBuffer.allocateDirect((int)remoteSize);
+        UcpMemory recvMemory = context.registerMemory(allocationParams);
+        resources.push(recvMemory);
+        ByteBuffer data = UcxUtils.getByteBufferView(recvMemory.getAddress(),
+            (int)Math.min(Integer.MAX_VALUE, totalSize));
         for (int i = 0; i < numIterations; i++) {
             final int iterNum = i;
-            UcxRequest getRequest = endpoint.getNonBlocking(remoteAddress, remoteKey, data,
+            UcxRequest getRequest = endpoint.getNonBlocking(remoteAddress, remoteKey,
+                recvMemory.getAddress(), totalSize,
                 new UcxCallback() {
                     long startTime = System.nanoTime();
 
@@ -73,7 +78,7 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
                         data.clear();
                         assert data.hashCode() == remoteHashCode;
                         double bw = getBandwithGbits(finishTime - startTime, remoteSize);
-                        System.out.printf("Iteration %d, bandwidth: %.4f GB/s\n", iterNum, bw);
+                        System.out.printf("Iteration %d, bandwidth: %.4f GB/s%n", iterNum, bw);
                     }
                 });
 

bindings/java/src/main/java/org/openucx/jucx/examples/UcxReadBWBenchmarkSender.java
@@ -7,18 +7,19 @@ package org.openucx.jucx.examples;
 
 import org.openucx.jucx.UcxCallback;
 import org.openucx.jucx.UcxRequest;
+import org.openucx.jucx.UcxUtils;
 import org.openucx.jucx.ucp.UcpEndpoint;
 import org.openucx.jucx.ucp.UcpEndpointParams;
 import org.openucx.jucx.ucp.UcpMemory;
+import org.openucx.jucx.ucp.UcpRegisterMemoryParams;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 
 
 public class UcxReadBWBenchmarkSender extends UcxBenchmark {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         if (!initializeArguments(args)) {
             return;
         }
@@ -29,28 +30,18 @@ public class UcxReadBWBenchmarkSender extends UcxBenchmark {
         UcpEndpoint endpoint = worker.newEndpoint(new UcpEndpointParams()
             .setSocketAddress(new InetSocketAddress(serverHost, serverPort)));
 
-        // In java ByteBuffer can be allocated up to 2GB (int max size).
-        if (totalSize >= Integer.MAX_VALUE) {
-            throw new IOException("Max size must be no greater then " + Integer.MAX_VALUE);
-        }
-        ByteBuffer data = ByteBuffer.allocateDirect(totalSize);
-        byte b = Byte.MIN_VALUE;
-        while (data.hasRemaining()) {
-            data.put(b++);
-        }
-        data.clear();
-
-        // Register allocated buffer
-        UcpMemory memory = context.registerMemory(data);
+        UcpMemory memory = context.registerMemory(allocationParams);
+        ByteBuffer data = UcxUtils.getByteBufferView(memory.getAddress(),
+            (int)Math.min(Integer.MAX_VALUE, totalSize));
 
         // Send worker and memory address and Rkey to receiver.
         ByteBuffer rkeyBuffer = memory.getRemoteKeyBuffer();
         ByteBuffer workerAddress = worker.getAddress();
 
-        ByteBuffer sendData = ByteBuffer.allocateDirect(24 + rkeyBuffer.capacity() +
+        ByteBuffer sendData = ByteBuffer.allocateDirect(28 + rkeyBuffer.capacity() +
             workerAddress.capacity());
         sendData.putLong(memory.getAddress());
-        sendData.putInt(totalSize);
+        sendData.putLong(totalSize);
         sendData.putInt(rkeyBuffer.capacity());
         sendData.put(rkeyBuffer);
         sendData.putInt(workerAddress.capacity());

bindings/java/src/test/java/org/openucx/jucx/UcpEndpointTest.java
@@ -8,6 +8,7 @@ package org.openucx.jucx;
 import org.junit.Test;
 import org.openucx.jucx.ucp.*;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
@@ -189,7 +190,7 @@ public class UcpEndpointTest {
     }
 
     @Test
-    public void testSendRecv() {
+    public void testSendRecv() throws Exception {
         // Crerate 2 contexts + 2 workers
         UcpParams params = new UcpParams().requestRmaFeature().requestTagFeature();
         UcpWorkerParams rdmaWorkerParams = new UcpWorkerParams().requestWakeupRMA();
@@ -199,8 +200,12 @@ public class UcpEndpointTest {
         UcpWorker worker2 = context2.newWorker(rdmaWorkerParams);
 
         // Allocate 2 source and 2 destination buffers, to perform 2 RDMA Read operations
-        ByteBuffer src1 = ByteBuffer.allocateDirect(UcpMemoryTest.MEM_SIZE);
-        ByteBuffer src2 = ByteBuffer.allocateDirect(UcpMemoryTest.MEM_SIZE);
+        UcpRegisterMemoryParams allocationParams = new UcpRegisterMemoryParams().allocate()
+            .setLength(UcpMemoryTest.MEM_SIZE);
+        UcpMemory memory1 = context1.registerMemory(allocationParams);
+        UcpMemory memory2 = context1.registerMemory(allocationParams);
+        ByteBuffer src1 = UcxUtils.getByteBufferView(memory1.getAddress(), UcpMemoryTest.MEM_SIZE);
+        ByteBuffer src2 = UcxUtils.getByteBufferView(memory1.getAddress(), UcpMemoryTest.MEM_SIZE);
         ByteBuffer dst1 = ByteBuffer.allocateDirect(UcpMemoryTest.MEM_SIZE);
         ByteBuffer dst2 = ByteBuffer.allocateDirect(UcpMemoryTest.MEM_SIZE);
         src1.asCharBuffer().put(UcpMemoryTest.RANDOM_TEXT);
@@ -235,6 +240,8 @@ public class UcpEndpointTest {
         }
 
         ep.close();
+        memory1.deregister();
+        memory2.deregister();
         worker1.close();
         worker2.close();
         context1.close();

bindings/java/src/test/java/org/openucx/jucx/UcpMemoryTest.java
@@ -20,31 +20,32 @@ import static java.nio.file.StandardOpenOption.*;
 import static org.junit.Assert.*;
 
 public class UcpMemoryTest {
-    public static int MEM_SIZE = 4096;
-    public static String RANDOM_TEXT = UUID.randomUUID().toString();
+    static int MEM_SIZE = 4096;
+    static String RANDOM_TEXT = UUID.randomUUID().toString();
 
     @Test
-    public void testMmapFile() throws IOException {
+    public void testMmapFile() throws Exception {
         UcpContext context = new UcpContext(new UcpParams().requestTagFeature());
         Path tempFile = Files.createTempFile("jucx", "test");
         // 1. Create FileChannel to file in tmp directory.
         FileChannel fileChannel = FileChannel.open(tempFile, CREATE, WRITE, READ, DELETE_ON_CLOSE);
-        ByteBuffer data = ByteBuffer.allocateDirect(MEM_SIZE);
-        // 2. Put to file some random text.
-        data.asCharBuffer().put(RANDOM_TEXT);
-        fileChannel.write(data);
-        fileChannel.force(true);
-        // 3. MMap file channel.
         MappedByteBuffer buf = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, MEM_SIZE);
-        buf.load();
-        // 4. Make sure content of mmaped file has needed data
-        assertEquals(RANDOM_TEXT, buf.asCharBuffer().toString().trim());
-        // 5. Register mmaped buffer with UCX
-        UcpMemory mem = context.registerMemory(buf);
-        ByteBuffer memBuffer = mem.getData();
-        // 6. Make sure registered buffer is the same as mapped buffer.
-        assertEquals(RANDOM_TEXT, memBuffer.asCharBuffer().toString().trim());
-        mem.deregister();
+        buf.asCharBuffer().put(RANDOM_TEXT);
+        buf.force();
+        // 2. Register mmap buffer with ODP
+        UcpMemory mmapedMemory = context.registerMemory(new UcpRegisterMemoryParams()
+            .setAddress(UcxUtils.getAddress(buf)).setLength(MEM_SIZE).nonBlocking());
+
+        assertEquals(mmapedMemory.getAddress(), UcxUtils.getAddress(buf));
+
+        // 3. Test allocation
+        UcpMemory allocatedMemory = context.registerMemory(new UcpRegisterMemoryParams()
+            .allocate().setLength(MEM_SIZE).nonBlocking());
+        assertEquals(allocatedMemory.getLength(), MEM_SIZE);
+
+
+        allocatedMemory.deregister();
+        mmapedMemory.deregister();
         fileChannel.close();
         context.close();
     }

openeuler-mirror/kucx

bindings/java/src/main/java/org/openucx/jucx/examples/UcxBenchmark.java
@@ -5,10 +5,7 @@
 
 package org.openucx.jucx.examples;
 
-import org.openucx.jucx.ucp.UcpContext;
-import org.openucx.jucx.ucp.UcpParams;
-import org.openucx.jucx.ucp.UcpWorker;
-import org.openucx.jucx.ucp.UcpWorkerParams;
+import org.openucx.jucx.ucp.*;
 
 import java.io.Closeable;
 import java.io.IOException;
@@ -32,7 +29,9 @@ public abstract class UcxBenchmark {
 
     protected static int numIterations;
 
-    protected static int totalSize;
+    protected static long totalSize;
+
+    protected static UcpMemMapParams allocationParams;
 
     private static String DESCRIPTION = "JUCX benchmark.\n" +
         "Run: \n" +
@@ -45,12 +44,14 @@ public abstract class UcxBenchmark {
         "s - IP address to bind sender listener (default: 0.0.0.0)\n" +
         "p - port to bind sender listener (default: 54321)\n" +
         "t - total size in bytes to transfer from sender to receiver (default 10000)\n" +
+        "o - on demand registration (default: false) \n" +
         "n - number of iterations (default 5)\n";
 
     static {
         argsMap.put("s", "0.0.0.0");
         argsMap.put("p", "54321");
         argsMap.put("t", "10000");
+        argsMap.put("o", "false");
         argsMap.put("n", "5");
     }
 
@@ -69,7 +70,11 @@ public abstract class UcxBenchmark {
         try {
             serverPort = Integer.parseInt(argsMap.get("p"));
             numIterations = Integer.parseInt(argsMap.get("n"));
-            totalSize = Integer.parseInt(argsMap.get("t"));
+            totalSize = Long.parseLong(argsMap.get("t"));
+            allocationParams = new UcpMemMapParams().allocate().setLength(totalSize);
+            if (argsMap.get("o").compareToIgnoreCase("true") == 0) {
+                allocationParams.nonBlocking();
+            }
         } catch (NumberFormatException ex) {
             System.out.println(DESCRIPTION);
             return false;

bindings/java/src/main/java/org/openucx/jucx/examples/UcxReadBWBenchmarkReceiver.java
@@ -7,16 +7,16 @@ package org.openucx.jucx.examples;
 
 import org.openucx.jucx.UcxCallback;
 import org.openucx.jucx.UcxRequest;
+import org.openucx.jucx.UcxUtils;
 import org.openucx.jucx.ucp.*;
 
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 
 public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         if (!initializeArguments(args)) {
             return;
         }
@@ -39,7 +39,7 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
         }
 
         long remoteAddress = recvBuffer.getLong();
-        long remoteSize = recvBuffer.getInt();
+        long remoteSize = recvBuffer.getLong();
         int remoteKeySize = recvBuffer.getInt();
         int rkeyBufferOffset = recvBuffer.position();
 
@@ -49,7 +49,7 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
         copyBuffer(recvBuffer, workerAddress, workerAdressSize);
 
         int remoteHashCode = recvBuffer.getInt();
-        System.out.printf("Received connection. Will read %d bytes from remote address %d\n",
+        System.out.printf("Received connection. Will read %d bytes from remote address %d%n",
             remoteSize, remoteAddress);
 
         UcpEndpoint endpoint = worker.newEndpoint(
@@ -60,10 +60,14 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
         UcpRemoteKey remoteKey = endpoint.unpackRemoteKey(recvBuffer);
         resources.push(remoteKey);
 
-        ByteBuffer data = ByteBuffer.allocateDirect((int)remoteSize);
+        UcpMemory recvMemory = context.memoryMap(allocationParams);
+        resources.push(recvMemory);
+        ByteBuffer data = UcxUtils.getByteBufferView(recvMemory.getAddress(),
+            (int)Math.min(Integer.MAX_VALUE, totalSize));
         for (int i = 0; i < numIterations; i++) {
             final int iterNum = i;
-            UcxRequest getRequest = endpoint.getNonBlocking(remoteAddress, remoteKey, data,
+            UcxRequest getRequest = endpoint.getNonBlocking(remoteAddress, remoteKey,
+                recvMemory.getAddress(), totalSize,
                 new UcxCallback() {
                     long startTime = System.nanoTime();
 
@@ -73,7 +77,7 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
                         data.clear();
                         assert data.hashCode() == remoteHashCode;
                         double bw = getBandwithGbits(finishTime - startTime, remoteSize);
-                        System.out.printf("Iteration %d, bandwidth: %.4f GB/s\n", iterNum, bw);
+                        System.out.printf("Iteration %d, bandwidth: %.4f GB/s%n", iterNum, bw);
                     }
                 });
 

bindings/java/src/main/java/org/openucx/jucx/examples/UcxReadBWBenchmarkSender.java
@@ -7,18 +7,18 @@ package org.openucx.jucx.examples;
 
 import org.openucx.jucx.UcxCallback;
 import org.openucx.jucx.UcxRequest;
+import org.openucx.jucx.UcxUtils;
 import org.openucx.jucx.ucp.UcpEndpoint;
 import org.openucx.jucx.ucp.UcpEndpointParams;
 import org.openucx.jucx.ucp.UcpMemory;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 
 
 public class UcxReadBWBenchmarkSender extends UcxBenchmark {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         if (!initializeArguments(args)) {
             return;
         }
@@ -29,28 +29,18 @@ public class UcxReadBWBenchmarkSender extends UcxBenchmark {
         UcpEndpoint endpoint = worker.newEndpoint(new UcpEndpointParams()
             .setSocketAddress(new InetSocketAddress(serverHost, serverPort)));
 
-        // In java ByteBuffer can be allocated up to 2GB (int max size).
-        if (totalSize >= Integer.MAX_VALUE) {
-            throw new IOException("Max size must be no greater then " + Integer.MAX_VALUE);
-        }
-        ByteBuffer data = ByteBuffer.allocateDirect(totalSize);
-        byte b = Byte.MIN_VALUE;
-        while (data.hasRemaining()) {
-            data.put(b++);
-        }
-        data.clear();
-
-        // Register allocated buffer
-        UcpMemory memory = context.registerMemory(data);
+        UcpMemory memory = context.memoryMap(allocationParams);
+        ByteBuffer data = UcxUtils.getByteBufferView(memory.getAddress(),
+            (int)Math.min(Integer.MAX_VALUE, totalSize));
 
         // Send worker and memory address and Rkey to receiver.
         ByteBuffer rkeyBuffer = memory.getRemoteKeyBuffer();
         ByteBuffer workerAddress = worker.getAddress();
 
-        ByteBuffer sendData = ByteBuffer.allocateDirect(24 + rkeyBuffer.capacity() +
+        ByteBuffer sendData = ByteBuffer.allocateDirect(28 + rkeyBuffer.capacity() +
             workerAddress.capacity());
         sendData.putLong(memory.getAddress());
-        sendData.putInt(totalSize);
+        sendData.putLong(totalSize);
         sendData.putInt(rkeyBuffer.capacity());
         sendData.put(rkeyBuffer);
         sendData.putInt(workerAddress.capacity());

bindings/java/src/test/java/org/openucx/jucx/UcpEndpointTest.java
@@ -189,7 +189,7 @@ public class UcpEndpointTest {
     }
 
     @Test
-    public void testSendRecv() {
+    public void testSendRecv() throws Exception {
         // Crerate 2 contexts + 2 workers
         UcpParams params = new UcpParams().requestRmaFeature().requestTagFeature();
         UcpWorkerParams rdmaWorkerParams = new UcpWorkerParams().requestWakeupRMA();
@@ -199,8 +199,12 @@ public class UcpEndpointTest {
         UcpWorker worker2 = context2.newWorker(rdmaWorkerParams);
 
         // Allocate 2 source and 2 destination buffers, to perform 2 RDMA Read operations
-        ByteBuffer src1 = ByteBuffer.allocateDirect(UcpMemoryTest.MEM_SIZE);
-        ByteBuffer src2 = ByteBuffer.allocateDirect(UcpMemoryTest.MEM_SIZE);
+        UcpMemMapParams allocationParams = new UcpMemMapParams().allocate()
+            .setLength(UcpMemoryTest.MEM_SIZE);
+        UcpMemory memory1 = context1.memoryMap(allocationParams);
+        UcpMemory memory2 = context1.memoryMap(allocationParams);
+        ByteBuffer src1 = UcxUtils.getByteBufferView(memory1.getAddress(), UcpMemoryTest.MEM_SIZE);
+        ByteBuffer src2 = UcxUtils.getByteBufferView(memory1.getAddress(), UcpMemoryTest.MEM_SIZE);
         ByteBuffer dst1 = ByteBuffer.allocateDirect(UcpMemoryTest.MEM_SIZE);
         ByteBuffer dst2 = ByteBuffer.allocateDirect(UcpMemoryTest.MEM_SIZE);
         src1.asCharBuffer().put(UcpMemoryTest.RANDOM_TEXT);
@@ -235,6 +239,8 @@ public class UcpEndpointTest {
         }
 
         ep.close();
+        memory1.deregister();
+        memory2.deregister();
         worker1.close();
         worker2.close();
         context1.close();

bindings/java/src/test/java/org/openucx/jucx/UcpMemoryTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 
 import org.openucx.jucx.ucp.*;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
@@ -20,31 +19,31 @@ import static java.nio.file.StandardOpenOption.*;
 import static org.junit.Assert.*;
 
 public class UcpMemoryTest {
-    public static int MEM_SIZE = 4096;
-    public static String RANDOM_TEXT = UUID.randomUUID().toString();
+    static int MEM_SIZE = 4096;
+    static String RANDOM_TEXT = UUID.randomUUID().toString();
 
     @Test
-    public void testMmapFile() throws IOException {
+    public void testMmapFile() throws Exception {
         UcpContext context = new UcpContext(new UcpParams().requestTagFeature());
         Path tempFile = Files.createTempFile("jucx", "test");
         // 1. Create FileChannel to file in tmp directory.
         FileChannel fileChannel = FileChannel.open(tempFile, CREATE, WRITE, READ, DELETE_ON_CLOSE);
-        ByteBuffer data = ByteBuffer.allocateDirect(MEM_SIZE);
-        // 2. Put to file some random text.
-        data.asCharBuffer().put(RANDOM_TEXT);
-        fileChannel.write(data);
-        fileChannel.force(true);
-        // 3. MMap file channel.
         MappedByteBuffer buf = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, MEM_SIZE);
-        buf.load();
-        // 4. Make sure content of mmaped file has needed data
-        assertEquals(RANDOM_TEXT, buf.asCharBuffer().toString().trim());
-        // 5. Register mmaped buffer with UCX
-        UcpMemory mem = context.registerMemory(buf);
-        ByteBuffer memBuffer = mem.getData();
-        // 6. Make sure registered buffer is the same as mapped buffer.
-        assertEquals(RANDOM_TEXT, memBuffer.asCharBuffer().toString().trim());
-        mem.deregister();
+        buf.asCharBuffer().put(RANDOM_TEXT);
+        buf.force();
+        // 2. Register mmap buffer with ODP
+        UcpMemory mmapedMemory = context.memoryMap(new UcpMemMapParams()
+            .setAddress(UcxUtils.getAddress(buf)).setLength(MEM_SIZE).nonBlocking());
+
+        assertEquals(mmapedMemory.getAddress(), UcxUtils.getAddress(buf));
+
+        // 3. Test allocation
+        UcpMemory allocatedMemory = context.memoryMap(new UcpMemMapParams()
+            .allocate().setLength(MEM_SIZE).nonBlocking());
+        assertEquals(allocatedMemory.getLength(), MEM_SIZE);
+
+        allocatedMemory.deregister();
+        mmapedMemory.deregister();
         fileChannel.close();
         context.close();
     }