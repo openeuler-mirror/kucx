openucx/ucx

JUCX: ep_close_nb functionality.

bindings/java/src/main/java/org/openucx/jucx/examples/UcxReadBWBenchmarkReceiver.java
@@ -34,9 +34,7 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
 
         System.out.println("Waiting for connections on " + sockaddr + " ...");
 
-        while (!recvRequest.isCompleted()) {
-            worker.progress();
-        }
+        worker.progressRequest(recvRequest);
 
         long remoteAddress = recvBuffer.getLong();
         long remoteSize = recvBuffer.getLong();
@@ -54,7 +52,6 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
 
         UcpEndpoint endpoint = worker.newEndpoint(
             new UcpEndpointParams().setUcpAddress(workerAddress).setPeerErrorHadnlingMode());
-
 
         recvBuffer.position(rkeyBufferOffset);
         UcpRemoteKey remoteKey = endpoint.unpackRemoteKey(recvBuffer);
@@ -81,9 +78,7 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
                     }
                 });
 
-            while (!getRequest.isCompleted()) {
-                worker.progress();
-            }
+            worker.progressRequest(getRequest);
             // To make sure we receive correct data each time to compare hashCodes
             data.put(0, (byte)1);
         }
@@ -92,17 +87,11 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
         sendBuffer.asCharBuffer().put("DONE");
         UcxRequest sent = endpoint.sendTaggedNonBlocking(sendBuffer, null);
 
-        while (!sent.isCompleted()) {
-            worker.progress();
-        }
+        worker.progressRequest(sent);
 
-        // Close endpoint and wait for remote side
-        // TODO remove when UCP close protocol is implemented
-        endpoint.close();
-        try {
-            Thread.sleep(3000);
-        } catch (java.lang.InterruptedException e) {
-        }
+        UcxRequest close = endpoint.closeNonBlockingFlush();
+
+        worker.progressRequest(close);
 
         closeResources();
     }

bindings/java/src/main/java/org/openucx/jucx/ucp/UcpEndpoint.java
@@ -198,6 +198,24 @@ public class UcpEndpoint extends UcxNativeStruct implements Closeable {
         return flushNonBlockingNative(getNativeId(), callback);
     }
 
+    /**
+     * Releases the endpoint without any confirmation from the peer. All
+     * outstanding requests will be completed with UCS_ERR_CANCELED error.
+     * This mode may cause transport level errors on remote side, so it requires set
+     * {@link UcpEndpointParams#setPeerErrorHadnlingMode()} for all endpoints created on
+     * both (local and remote) sides to avoid undefined behavior.
+     */
+    public UcxRequest closeNonBlockingForce() {
+        return closeNonBlockingNative(getNativeId(), UcpConstants.UCP_EP_CLOSE_MODE_FORCE);
+    }
+
+    /**
+     * Releases the endpoint by scheduling flushes on all outstanding operations.
+     */
+    public UcxRequest closeNonBlockingFlush() {
+        return closeNonBlockingNative(getNativeId(), UcpConstants.UCP_EP_CLOSE_MODE_FLUSH);
+    }
+
     private static native long createEndpointNative(UcpEndpointParams params, long workerId);
 
     private static native void destroyEndpointNative(long epId);
@@ -225,4 +243,6 @@ public class UcpEndpoint extends UcxNativeStruct implements Closeable {
                                                                  UcxCallback callback);
 
     private static native UcxRequest flushNonBlockingNative(long enpointId, UcxCallback callback);
+
+    private static native UcxRequest closeNonBlockingNative(long endpointId, int mode);
 }

bindings/java/src/main/java/org/openucx/jucx/ucp/UcpWorker.java
@@ -64,6 +64,15 @@ public class UcpWorker extends UcxNativeStruct implements Closeable {
     }
 
     /**
+     * Blocking progress for request until it's not completed.
+     */
+    public void progressRequest(UcxRequest request) {
+        while (!request.isCompleted()) {
+            progress();
+        }
+    }
+
+    /**
      * This routine flushes all outstanding AMO and RMA communications on the
      * this worker. All the AMO and RMA operations issued on this  worker prior to this call
      * are completed both at the origin and at the target when this call returns.

openeuler-mirror/kucx

bindings/java/src/main/java/org/openucx/jucx/examples/UcxReadBWBenchmarkReceiver.java
@@ -34,9 +34,7 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
 
         System.out.println("Waiting for connections on " + sockaddr + " ...");
 
-        while (!recvRequest.isCompleted()) {
-            worker.progress();
-        }
+        worker.progressRequest(recvRequest);
 
         long remoteAddress = recvBuffer.getLong();
         long remoteSize = recvBuffer.getLong();
@@ -54,7 +52,6 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
 
         UcpEndpoint endpoint = worker.newEndpoint(
             new UcpEndpointParams().setUcpAddress(workerAddress).setPeerErrorHadnlingMode());
-
 
         recvBuffer.position(rkeyBufferOffset);
         UcpRemoteKey remoteKey = endpoint.unpackRemoteKey(recvBuffer);
@@ -81,28 +78,19 @@ public class UcxReadBWBenchmarkReceiver extends UcxBenchmark {
                     }
                 });
 
-            while (!getRequest.isCompleted()) {
-                worker.progress();
-            }
+            worker.progressRequest(getRequest);
             // To make sure we receive correct data each time to compare hashCodes
             data.put(0, (byte)1);
         }
 
         ByteBuffer sendBuffer = ByteBuffer.allocateDirect(100);
         sendBuffer.asCharBuffer().put("DONE");
+        
         UcpRequest sent = endpoint.sendTaggedNonBlocking(sendBuffer, null);
+        worker.progressRequest(sent);
 
-        while (!sent.isCompleted()) {
-            worker.progress();
-        }
-
-        // Close endpoint and wait for remote side
-        // TODO remove when UCP close protocol is implemented
-        endpoint.close();
-        try {
-            Thread.sleep(3000);
-        } catch (java.lang.InterruptedException e) {
-        }
+        UcpRequest close = endpoint.closeNonBlockingFlush();
+        worker.progressRequest(close);
 
         closeResources();
     }

bindings/java/src/main/java/org/openucx/jucx/ucp/UcpEndpoint.java
@@ -198,6 +198,24 @@ public class UcpEndpoint extends UcxNativeStruct implements Closeable {
         return flushNonBlockingNative(getNativeId(), callback);
     }
 
+    /**
+     * Releases the endpoint without any confirmation from the peer. All
+     * outstanding requests will be completed with UCS_ERR_CANCELED error.
+     * This mode may cause transport level errors on remote side, so it requires set
+     * {@link UcpEndpointParams#setPeerErrorHadnlingMode()} for all endpoints created on
+     * both (local and remote) sides to avoid undefined behavior.
+     */
+    public UcpRequest closeNonBlockingForce() {
+        return closeNonBlockingNative(getNativeId(), UcpConstants.UCP_EP_CLOSE_MODE_FORCE);
+    }
+
+    /**
+     * Releases the endpoint by scheduling flushes on all outstanding operations.
+     */
+    public UcpRequest closeNonBlockingFlush() {
+        return closeNonBlockingNative(getNativeId(), UcpConstants.UCP_EP_CLOSE_MODE_FLUSH);
+    }
+
     private static native long createEndpointNative(UcpEndpointParams params, long workerId);
 
     private static native void destroyEndpointNative(long epId);
@@ -225,4 +243,6 @@ public class UcpEndpoint extends UcxNativeStruct implements Closeable {
                                                                  UcxCallback callback);
 
     private static native UcpRequest flushNonBlockingNative(long enpointId, UcxCallback callback);
+
+    private static native UcpRequest closeNonBlockingNative(long endpointId, int mode);
 }

bindings/java/src/main/java/org/openucx/jucx/ucp/UcpWorker.java
@@ -64,6 +64,15 @@ public class UcpWorker extends UcxNativeStruct implements Closeable {
     }
 
     /**
+     * Blocking progress for request until it's not completed.
+     */
+    public void progressRequest(UcpRequest request) {
+        while (!request.isCompleted()) {
+            progress();
+        }
+    }
+
+    /**
      * This routine flushes all outstanding AMO and RMA communications on the
      * this worker. All the AMO and RMA operations issued on this  worker prior to this call
      * are completed both at the origin and at the target when this call returns.