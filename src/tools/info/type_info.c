openucx/ucx

UCP/CORE/WIREUP: Use UCS conn match

src/tools/info/type_info.c
@@ -124,6 +124,7 @@ void print_type_info(const char * tl_name)
         PRINT_SIZE(ucs_pgt_region_t);
         PRINT_SIZE(ucs_rcache_t);
         PRINT_SIZE(ucs_rcache_region_t);
+        PRINT_SIZE(ucs_conn_match_entry_t);
 
         printf("\nUCT:\n");
         PRINT_SIZE(uct_am_handler_t);
@@ -259,7 +260,6 @@ void print_type_info(const char * tl_name)
     PRINT_SIZE(ucp_ep_t);
     PRINT_SIZE(ucp_ep_ext_gen_t);
     PRINT_SIZE(ucp_ep_ext_proto_t);
-    PRINT_SIZE(ucp_ep_match_entry_t);
     PRINT_SIZE(ucp_ep_config_key_t);
     PRINT_SIZE(ucp_ep_config_t);
     PRINT_SIZE(ucp_request_t);

src/ucp/core/ucp_ep.c
@@ -102,7 +102,7 @@ ucs_status_t ucp_ep_create_base(ucp_worker_h worker, const char *peer_name,
     ep->worker                      = worker;
     ep->am_lane                     = UCP_NULL_LANE;
     ep->flags                       = 0;
-    ep->conn_sn                     = (ucp_ep_conn_sn_t)-1;
+    ep->conn_sn                     = (ucs_conn_sn_t)-1;
     ucp_ep_ext_gen(ep)->user_data   = NULL;
     ucp_ep_ext_gen(ep)->dest_ep_ptr = 0;
     ucp_ep_ext_gen(ep)->err_cb      = NULL;
@@ -564,7 +564,7 @@ ucp_ep_create_api_to_worker_addr(ucp_worker_h worker,
                                  const ucp_ep_params_t *params, ucp_ep_h *ep_p)
 {
     ucp_unpacked_address_t remote_address;
-    ucp_ep_conn_sn_t conn_sn;
+    ucs_conn_sn_t conn_sn;
     ucs_status_t status;
     unsigned flags;
     ucp_ep_h ep;
@@ -594,9 +594,10 @@ ucp_ep_create_api_to_worker_addr(ucp_worker_h worker,
      * dst_ep != 0. So, ucp_wireup_request() will not create an unexpected ep
      * in ep_match.
      */
-    conn_sn = ucp_ep_match_get_next_sn(&worker->ep_match_ctx, remote_address.uuid);
-    ep = ucp_ep_match_retrieve_unexp(&worker->ep_match_ctx, remote_address.uuid,
-                                     conn_sn ^ (remote_address.uuid == worker->uuid));
+    conn_sn = ucp_worker_ep_match_get_sn(worker, remote_address.uuid);
+    ep      = ucp_worker_ep_match_retrieve(worker, remote_address.uuid,
+                                           conn_sn ^
+                                           (remote_address.uuid == worker->uuid), 0);
     if (ep != NULL) {
         status = ucp_ep_adjust_params(ep, params);
         if (status != UCS_OK) {
@@ -635,7 +636,8 @@ ucp_ep_create_api_to_worker_addr(ucp_worker_h worker,
         ucp_ep_update_dest_ep_ptr(ep, (uintptr_t)ep);
         ucp_ep_flush_state_reset(ep);
     } else {
-        ucp_ep_match_insert_exp(&worker->ep_match_ctx, remote_address.uuid, ep);
+        ucp_worker_ep_match_insert(worker, ep, remote_address.uuid,
+                                   conn_sn, 1);
     }
 
     /* if needed, send initial wireup message */
@@ -796,7 +798,7 @@ void ucp_ep_disconnected(ucp_ep_h ep, int force)
         return;
     }
 
-    ucp_ep_match_remove_ep(&ep->worker->ep_match_ctx, ep);
+    ucp_worker_ep_match_remove_ep(ep->worker, ep);
     ucp_ep_destroy_internal(ep);
 }
 

src/ucp/wireup/wireup.c
@@ -437,8 +437,9 @@ ucp_wireup_process_request(ucp_worker_h worker, const ucp_wireup_msg_t *msg,
         }
         ep_init_flags |= UCP_EP_INIT_CREATE_AM_LANE;
     } else {
-        ep = ucp_ep_match_retrieve_exp(&worker->ep_match_ctx, remote_uuid,
-                                       msg->conn_sn ^ (remote_uuid == worker->uuid));
+        ep = ucp_worker_ep_match_retrieve(worker, remote_uuid,
+                                          msg->conn_sn ^
+                                          (remote_uuid == worker->uuid), 1);
         if (ep == NULL) {
             /* Create a new endpoint if does not exist */
             status = ucp_worker_create_ep(worker, remote_address->name,
@@ -449,7 +450,8 @@ ucp_wireup_process_request(ucp_worker_h worker, const ucp_wireup_msg_t *msg,
 
             /* add internal endpoint to hash */
             ep->conn_sn = msg->conn_sn;
-            ucp_ep_match_insert_unexp(&worker->ep_match_ctx, remote_uuid, ep);
+            ucp_worker_ep_match_insert(worker, ep, remote_uuid,
+                                       ep->conn_sn, 0);
         } else {
             ucp_ep_flush_state_reset(ep);
         }
@@ -578,7 +580,7 @@ ucp_wireup_process_reply(ucp_worker_h worker, const ucp_wireup_msg_t *msg,
     ucs_trace("ep %p: got wireup reply src_ep 0x%lx dst_ep 0x%lx sn %d", ep,
               msg->src_ep_ptr, msg->dest_ep_ptr, msg->conn_sn);
 
-    ucp_ep_match_remove_ep(&worker->ep_match_ctx, ep);
+    ucp_worker_ep_match_remove_ep(worker, ep);
     ucp_ep_update_dest_ep_ptr(ep, msg->src_ep_ptr);
     ucp_ep_flush_state_reset(ep);
 

src/ucp/wireup/wireup.h
@@ -71,7 +71,7 @@ typedef struct {
 typedef struct ucp_wireup_msg {
     uint8_t                 type;         /* Message type */
     ucp_err_handling_mode_t err_mode;     /* Peer error handling mode */
-    ucp_ep_conn_sn_t        conn_sn;      /* Connection sequence number */
+    ucs_conn_sn_t           conn_sn;      /* Connection sequence number */
     uintptr_t               src_ep_ptr;   /* Endpoint of source */
     uintptr_t               dest_ep_ptr;  /* Endpoint of destination (0 - invalid) */
     /* packed addresses follow */

openeuler-mirror/kucx

src/tools/info/type_info.c
@@ -124,6 +124,7 @@ void print_type_info(const char * tl_name)
         PRINT_SIZE(ucs_pgt_region_t);
         PRINT_SIZE(ucs_rcache_t);
         PRINT_SIZE(ucs_rcache_region_t);
+        PRINT_SIZE(ucs_conn_match_elem_t);
 
         printf("\nUCT:\n");
         PRINT_SIZE(uct_am_handler_t);
@@ -259,7 +260,6 @@ void print_type_info(const char * tl_name)
     PRINT_SIZE(ucp_ep_t);
     PRINT_SIZE(ucp_ep_ext_gen_t);
     PRINT_SIZE(ucp_ep_ext_proto_t);
-    PRINT_SIZE(ucp_ep_match_entry_t);
     PRINT_SIZE(ucp_ep_config_key_t);
     PRINT_SIZE(ucp_ep_config_t);
     PRINT_SIZE(ucp_request_t);

src/ucp/core/ucp_ep.c
@@ -102,7 +102,7 @@ ucs_status_t ucp_ep_create_base(ucp_worker_h worker, const char *peer_name,
     ep->worker                      = worker;
     ep->am_lane                     = UCP_NULL_LANE;
     ep->flags                       = 0;
-    ep->conn_sn                     = (ucp_ep_conn_sn_t)-1;
+    ep->conn_sn                     = UCP_EP_MATCH_CONN_SN_MAX;
     ucp_ep_ext_gen(ep)->user_data   = NULL;
     ucp_ep_ext_gen(ep)->dest_ep_ptr = 0;
     ucp_ep_ext_gen(ep)->err_cb      = NULL;
@@ -564,7 +564,7 @@ ucp_ep_create_api_to_worker_addr(ucp_worker_h worker,
                                  const ucp_ep_params_t *params, ucp_ep_h *ep_p)
 {
     ucp_unpacked_address_t remote_address;
-    ucp_ep_conn_sn_t conn_sn;
+    ucp_ep_match_conn_sn_t conn_sn;
     ucs_status_t status;
     unsigned flags;
     ucp_ep_h ep;
@@ -594,9 +594,10 @@ ucp_ep_create_api_to_worker_addr(ucp_worker_h worker,
      * dst_ep != 0. So, ucp_wireup_request() will not create an unexpected ep
      * in ep_match.
      */
-    conn_sn = ucp_ep_match_get_next_sn(&worker->ep_match_ctx, remote_address.uuid);
-    ep = ucp_ep_match_retrieve_unexp(&worker->ep_match_ctx, remote_address.uuid,
-                                     conn_sn ^ (remote_address.uuid == worker->uuid));
+    conn_sn = ucp_ep_match_get_sn(worker, remote_address.uuid);
+    ep      = ucp_ep_match_retrieve(worker, remote_address.uuid,
+                                    conn_sn ^
+                                    (remote_address.uuid == worker->uuid), 0);
     if (ep != NULL) {
         status = ucp_ep_adjust_params(ep, params);
         if (status != UCS_OK) {
@@ -635,7 +636,7 @@ ucp_ep_create_api_to_worker_addr(ucp_worker_h worker,
         ucp_ep_update_dest_ep_ptr(ep, (uintptr_t)ep);
         ucp_ep_flush_state_reset(ep);
     } else {
-        ucp_ep_match_insert_exp(&worker->ep_match_ctx, remote_address.uuid, ep);
+        ucp_ep_match_insert(worker, ep, remote_address.uuid, conn_sn, 1);
     }
 
     /* if needed, send initial wireup message */
@@ -796,7 +797,7 @@ void ucp_ep_disconnected(ucp_ep_h ep, int force)
         return;
     }
 
-    ucp_ep_match_remove_ep(&ep->worker->ep_match_ctx, ep);
+    ucp_ep_match_remove_ep(ep->worker, ep);
     ucp_ep_destroy_internal(ep);
 }
 

src/ucp/wireup/wireup.c
@@ -380,7 +380,7 @@ ucp_wireup_process_pre_request(ucp_worker_h worker, const ucp_wireup_msg_t *msg,
 
     ucs_assert(msg->type == UCP_WIREUP_MSG_PRE_REQUEST);
     ucs_assert(msg->dest_ep_ptr != 0);
-    ucs_trace("got wireup pre_request from 0x%"PRIx64" src_ep 0x%lx dst_ep 0x%lx conn_sn %d",
+    ucs_trace("got wireup pre_request from 0x%"PRIx64" src_ep 0x%lx dst_ep 0x%lx conn_sn %u",
               remote_address->uuid, msg->src_ep_ptr, msg->dest_ep_ptr, msg->conn_sn);
 
     /* wireup pre_request for a specific ep */
@@ -437,8 +437,9 @@ ucp_wireup_process_request(ucp_worker_h worker, const ucp_wireup_msg_t *msg,
         }
         ep_init_flags |= UCP_EP_INIT_CREATE_AM_LANE;
     } else {
-        ep = ucp_ep_match_retrieve_exp(&worker->ep_match_ctx, remote_uuid,
-                                       msg->conn_sn ^ (remote_uuid == worker->uuid));
+        ep = ucp_ep_match_retrieve(worker, remote_uuid,
+                                   msg->conn_sn ^
+                                   (remote_uuid == worker->uuid), 1);
         if (ep == NULL) {
             /* Create a new endpoint if does not exist */
             status = ucp_worker_create_ep(worker, remote_address->name,
@@ -449,7 +450,7 @@ ucp_wireup_process_request(ucp_worker_h worker, const ucp_wireup_msg_t *msg,
 
             /* add internal endpoint to hash */
             ep->conn_sn = msg->conn_sn;
-            ucp_ep_match_insert_unexp(&worker->ep_match_ctx, remote_uuid, ep);
+            ucp_ep_match_insert(worker, ep, remote_uuid, ep->conn_sn, 0);
         } else {
             ucp_ep_flush_state_reset(ep);
         }
@@ -578,7 +579,7 @@ ucp_wireup_process_reply(ucp_worker_h worker, const ucp_wireup_msg_t *msg,
     ucs_trace("ep %p: got wireup reply src_ep 0x%lx dst_ep 0x%lx sn %d", ep,
               msg->src_ep_ptr, msg->dest_ep_ptr, msg->conn_sn);
 
-    ucp_ep_match_remove_ep(&worker->ep_match_ctx, ep);
+    ucp_ep_match_remove_ep(worker, ep);
     ucp_ep_update_dest_ep_ptr(ep, msg->src_ep_ptr);
     ucp_ep_flush_state_reset(ep);
 

src/ucp/wireup/wireup.h
@@ -71,7 +71,7 @@ typedef struct {
 typedef struct ucp_wireup_msg {
     uint8_t                 type;         /* Message type */
     ucp_err_handling_mode_t err_mode;     /* Peer error handling mode */
-    ucp_ep_conn_sn_t        conn_sn;      /* Connection sequence number */
+    ucp_ep_match_conn_sn_t  conn_sn;      /* Connection sequence number */
     uintptr_t               src_ep_ptr;   /* Endpoint of source */
     uintptr_t               dest_ep_ptr;  /* Endpoint of destination (0 - invalid) */
     /* packed addresses follow */