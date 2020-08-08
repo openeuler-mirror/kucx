hiroyuki-sato/ucx

UCT: Use portable format string like PRIx64.

src/uct/tcp/sockcm/sockcm_iface.c
@@ -295,7 +295,7 @@ static UCS_CLASS_INIT_FUNC(uct_sockcm_iface_t, uct_md_h md, uct_worker_h worker,
 
     UCT_CHECK_PARAM((params->open_mode & UCT_IFACE_OPEN_MODE_SOCKADDR_SERVER) ||
                     (params->open_mode & UCT_IFACE_OPEN_MODE_SOCKADDR_CLIENT),
-                    "Invalid open mode %zu", params->open_mode);
+                    "Invalid open mode %"PRIx64, params->open_mode);
 
     UCT_CHECK_PARAM(!(params->open_mode & UCT_IFACE_OPEN_MODE_SOCKADDR_SERVER) ||
                     (params->field_mask & UCT_IFACE_PARAM_FIELD_SOCKADDR),

src/uct/tcp/tcp_ep.c
@@ -1150,7 +1150,7 @@ static unsigned uct_tcp_ep_progress_magic_number_rx(uct_tcp_ep_t *ep)
     if (magic_number != UCT_TCP_MAGIC_NUMBER) {
         /* Silently close this connection and destroy its EP */
         ucs_debug("tcp_iface %p (%s): received wrong magic number (expected: "
-                  "%zu, received: %zu) for ep=%p (fd=%d) from %s", iface,
+                  "%zu, received: %"PRIx64 ") for ep=%p (fd=%d) from %s", iface,
                   ucs_sockaddr_str((const struct sockaddr*)&iface->config.ifaddr,
                                    str_local_addr, UCS_SOCKADDR_STRING_LEN),
                   UCT_TCP_MAGIC_NUMBER, magic_number, ep,

src/uct/tcp/tcp_sockcm_ep.c
@@ -484,8 +484,9 @@ static ucs_status_t uct_tcp_sockcm_ep_server_invoke_conn_req_cb(uct_tcp_sockcm_e
     conn_req_args.client_address = client_saddr;
     ucs_strncpy_safe(conn_req_args.dev_name, ifname_str, UCT_DEVICE_NAME_MAX);
 
-    ucs_debug("fd %d: remote_data: (field_mask=%zu) dev_addr: %s (length=%zu), "
-              "conn_priv_data_length=%zu", cep->fd, remote_data.field_mask,
+    ucs_debug("fd %d: remote_data: (field_mask=%"PRIx64 ") "
+              "dev_addr: %s (length=%zu), conn_priv_data_length=%zu",
+              cep->fd, remote_data.field_mask,
               ucs_sockaddr_str((const struct sockaddr*)remote_data.dev_addr,
                                peer_str, UCS_SOCKADDR_STRING_LEN),
               remote_data.dev_addr_length, remote_data.conn_priv_data_length);
@@ -745,7 +746,7 @@ static ucs_status_t uct_tcp_sockcm_ep_server_create(uct_tcp_sockcm_ep_t *tcp_ep,
     ucs_status_t status;
 
     if (!(params->field_mask & UCT_EP_PARAM_FIELD_CM)) {
-        ucs_error("UCT_EP_PARAM_FIELD_CM is not set. field_mask 0x%lx",
+        ucs_error("UCT_EP_PARAM_FIELD_CM is not set. field_mask 0x%"PRIx64,
                   params->field_mask);
         status = UCS_ERR_INVALID_PARAM;
         goto err;

openeuler-mirror/kucx

src/uct/tcp/sockcm/sockcm_iface.c
@@ -295,7 +295,7 @@ static UCS_CLASS_INIT_FUNC(uct_sockcm_iface_t, uct_md_h md, uct_worker_h worker,
 
     UCT_CHECK_PARAM((params->open_mode & UCT_IFACE_OPEN_MODE_SOCKADDR_SERVER) ||
                     (params->open_mode & UCT_IFACE_OPEN_MODE_SOCKADDR_CLIENT),
-                    "Invalid open mode %zu", params->open_mode);
+                    "Invalid open mode %"PRIu64, params->open_mode);
 
     UCT_CHECK_PARAM(!(params->open_mode & UCT_IFACE_OPEN_MODE_SOCKADDR_SERVER) ||
                     (params->field_mask & UCT_IFACE_PARAM_FIELD_SOCKADDR),

src/uct/tcp/tcp_ep.c
@@ -1117,7 +1117,7 @@ static unsigned uct_tcp_ep_progress_magic_number_rx(uct_tcp_ep_t *ep)
     if (magic_number != UCT_TCP_MAGIC_NUMBER) {
         /* Silently close this connection and destroy its EP */
         ucs_debug("tcp_iface %p (%s): received wrong magic number (expected: "
-                  "%zu, received: %zu) for ep=%p (fd=%d) from %s", iface,
+                  "%zu, received: %"PRIu64 ") for ep=%p (fd=%d) from %s", iface,
                   ucs_sockaddr_str((const struct sockaddr*)&iface->config.ifaddr,
                                    str_local_addr, UCS_SOCKADDR_STRING_LEN),
                   UCT_TCP_MAGIC_NUMBER, magic_number, ep,

src/uct/tcp/tcp_sockcm_ep.c
@@ -508,8 +508,9 @@ static ucs_status_t uct_tcp_sockcm_ep_server_invoke_conn_req_cb(uct_tcp_sockcm_e
     conn_req_args.client_address = client_saddr;
     ucs_strncpy_safe(conn_req_args.dev_name, ifname_str, UCT_DEVICE_NAME_MAX);
 
-    ucs_debug("fd %d: remote_data: (field_mask=%zu) dev_addr: %s (length=%zu), "
-              "conn_priv_data_length=%zu", cep->fd, remote_data.field_mask,
+    ucs_debug("fd %d: remote_data: (field_mask=%"PRIu64 ") "
+              "dev_addr: %s (length=%zu), conn_priv_data_length=%zu",
+              cep->fd, remote_data.field_mask,
               ucs_sockaddr_str((const struct sockaddr*)remote_data.dev_addr,
                                peer_str, UCS_SOCKADDR_STRING_LEN),
               remote_data.dev_addr_length, remote_data.conn_priv_data_length);
@@ -783,7 +784,7 @@ static ucs_status_t uct_tcp_sockcm_ep_server_create(uct_tcp_sockcm_ep_t *tcp_ep,
     ucs_status_t status;
 
     if (!(params->field_mask & UCT_EP_PARAM_FIELD_CM)) {
-        ucs_error("UCT_EP_PARAM_FIELD_CM is not set. field_mask 0x%lx",
+        ucs_error("UCT_EP_PARAM_FIELD_CM is not set. field_mask 0x%"PRIx64,
                   params->field_mask);
         status = UCS_ERR_INVALID_PARAM;
         goto err;