openucx/ucx

UCS: new ptr_array API for bulk allocation
Signed-off-by: Alex Margolin <alex.margolin@huawei.com>

src/ucs/datastruct/ptr_array.c
@@ -13,6 +13,7 @@
 
 #include <ucs/sys/string.h>
 #include <ucs/sys/sys.h>
+#include <ucs/debug/memtrack_int.h>
 #include <ucs/debug/assert.h>
 #include <ucs/debug/log.h>
 
@@ -120,11 +121,11 @@ void ucs_ptr_array_init(ucs_ptr_array_t *ptr_array, const char *name)
     ptr_array->name = name;
 }
 
-void ucs_ptr_array_cleanup(ucs_ptr_array_t *ptr_array)
+void ucs_ptr_array_cleanup(ucs_ptr_array_t *ptr_array, int leak_check)
 {
     unsigned i;
 
-    if (ptr_array->count > 0) {
+    if (leak_check && (ptr_array->count > 0)) {
         ucs_warn("releasing ptr_array with %u used items", ptr_array->count);
         for (i = 0; i < ptr_array->size; ++i) {
             if (!ucs_ptr_array_is_free(ptr_array, i)) {
@@ -202,11 +203,51 @@ unsigned ucs_ptr_array_insert(ucs_ptr_array_t *ptr_array, void *value)
     return element_index;
 }
 
+unsigned ucs_ptr_array_alloc(ucs_ptr_array_t *ptr_array, unsigned element_count)
+{
+    unsigned free_iter, new_size, element_index;
+
+    if (element_count == 0) {
+        return 0;
+    }
+
+    free_iter     = 1;
+    element_index = ptr_array->freelist;
+    while (element_index != UCS_PTR_ARRAY_SENTINEL) {
+        while ((free_iter < element_count) &&
+               (ucs_ptr_array_is_free(ptr_array, element_index + free_iter))) {
+            free_iter++;
+        }
+
+        if (free_iter == element_count) {
+            goto alloc_init;
+        }
+
+        free_iter     = ptr_array->start[element_index];
+        element_index = ucs_ptr_array_freelist_get_next(free_iter);
+        free_iter     = 1;
+    }
+
+    element_index = ptr_array->size;
+    new_size = ucs_max(2 * ptr_array->size, ptr_array->size + element_count);
+    ucs_ptr_array_grow(ptr_array, new_size);
+
+alloc_init:
+    for (free_iter = 0; free_iter < element_count; free_iter++) {
+        /* set the value and remove from the free-list */
+        ucs_ptr_array_set(ptr_array, element_index + free_iter, 0);
+    }
+
+    return element_index;
+}
+
 void ucs_ptr_array_set(ucs_ptr_array_t *ptr_array, unsigned element_index,
                        void *new_val)
 {
     ucs_ptr_array_elem_t *elem;
     unsigned next, free_iter, free_ahead, new_size;
+
+    ucs_assert_always(((uintptr_t)new_val & UCS_PTR_ARRAY_FLAG_FREE) == 0);
 
     if (ucs_unlikely(element_index >= ptr_array->size)) {
         new_size = ucs_max(ptr_array->size * 2, element_index + 1);
@@ -303,11 +344,12 @@ ucs_ptr_array_locked_init(ucs_ptr_array_locked_t *locked_ptr_array,
     return UCS_OK;
 }
 
-void ucs_ptr_array_locked_cleanup(ucs_ptr_array_locked_t *locked_ptr_array)
+void ucs_ptr_array_locked_cleanup(ucs_ptr_array_locked_t *locked_ptr_array,
+                                  int leak_check)
 {
     ucs_recursive_spin_lock(&locked_ptr_array->lock);
     /* Call unlocked function */
-    ucs_ptr_array_cleanup(&locked_ptr_array->super);
+    ucs_ptr_array_cleanup(&locked_ptr_array->super, leak_check);
     ucs_recursive_spin_unlock(&locked_ptr_array->lock);
 
     /* Destroy spinlock */
@@ -322,6 +364,20 @@ unsigned ucs_ptr_array_locked_insert(ucs_ptr_array_locked_t *locked_ptr_array,
     ucs_recursive_spin_lock(&locked_ptr_array->lock);
     /* Call unlocked function */
     element_index = ucs_ptr_array_insert(&locked_ptr_array->super, value);
+    ucs_recursive_spin_unlock(&locked_ptr_array->lock);
+
+    return element_index;
+}
+
+unsigned ucs_ptr_array_locked_alloc(ucs_ptr_array_locked_t *locked_ptr_array,
+                                    unsigned element_count)
+{
+    unsigned element_index;
+
+    ucs_recursive_spin_lock(&locked_ptr_array->lock);
+    /* Call unlocked function */
+    element_index = ucs_ptr_array_alloc(&locked_ptr_array->super,
+                                        element_count);
     ucs_recursive_spin_unlock(&locked_ptr_array->lock);
 
     return element_index;

test/gtest/ucs/test_datatype.cc
@@ -6,7 +6,9 @@
 */
 
 #include <common/test.h>
+
 extern "C" {
+#include <ucs/debug/memtrack.h>
 #include <ucs/datastruct/array.inl>
 #include <ucs/datastruct/list.h>
 #include <ucs/datastruct/hlist.h>
@@ -542,6 +544,14 @@ UCS_TEST_F(test_datatype, ptr_array_basic) {
 
     ucs_ptr_array_set(&pa, 100, &g);
 
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 7);
+
+    index = ucs_ptr_array_alloc(&pa, 200);
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 207);
+
+    EXPECT_EQ(301u, pa.size);
+    EXPECT_EQ(101u, index);
+
     void *vc;
     int present = ucs_ptr_array_lookup(&pa, 2, vc);
     ASSERT_TRUE(present);
@@ -563,12 +573,16 @@ UCS_TEST_F(test_datatype, ptr_array_basic) {
     present = ucs_ptr_array_lookup(&pa, 100, vc);
     EXPECT_EQ(&g, vc);
 
+    present = ucs_ptr_array_lookup(&pa, 101, vc);
+    EXPECT_EQ(NULL, vc);
+
+    present = ucs_ptr_array_lookup(&pa, 301, vc);
+    EXPECT_EQ(NULL, vc);
+
     EXPECT_FALSE(ucs_ptr_array_lookup(&pa, 5, vc));
     EXPECT_FALSE(ucs_ptr_array_lookup(&pa, 99, vc));
-    EXPECT_FALSE(ucs_ptr_array_lookup(&pa, 101, vc));
+    EXPECT_FALSE(ucs_ptr_array_lookup(&pa, 302, vc));
     EXPECT_FALSE(ucs_ptr_array_lookup(&pa, 5005, vc));
-
-    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 7);
 
     ucs_ptr_array_remove(&pa, 0);
     ucs_ptr_array_remove(&pa, 1);
@@ -577,15 +591,64 @@ UCS_TEST_F(test_datatype, ptr_array_basic) {
     ucs_ptr_array_remove(&pa, 4);
     ucs_ptr_array_remove(&pa, 6);
 
-    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 1);
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 201);
     EXPECT_EQ(ucs_ptr_array_is_empty(&pa), 0);
 
-    ucs_ptr_array_remove(&pa, 100);
+    for (index = 100; index <= 300; index++) {
+        ucs_ptr_array_remove(&pa, index);
+    }
 
     EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 0);
     EXPECT_EQ(ucs_ptr_array_is_empty(&pa), 1);
 
-    ucs_ptr_array_cleanup(&pa);
+    ucs_ptr_array_cleanup(&pa, 1);
+}
+
+UCS_TEST_F(test_datatype, ptr_array_alloc) {
+    ucs_ptr_array_t pa;
+    unsigned idx, alloc1, alloc2;
+
+    ucs_ptr_array_init(&pa, "ptr_array alloc test");
+
+    alloc1 = ucs_ptr_array_alloc(&pa, 10);
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 10);
+
+    EXPECT_GE(pa.size, 10u);
+    EXPECT_EQ(alloc1, 0u);
+
+    alloc2 = ucs_ptr_array_alloc(&pa, 100);
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 110);
+
+    EXPECT_GE(pa.size, 110u);
+    EXPECT_EQ(alloc2, alloc1 + 10u);
+
+    for (idx = 0; idx < alloc2; idx++) {
+        ucs_ptr_array_remove(&pa, idx);
+    }
+
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 100);
+    EXPECT_EQ(alloc1, ucs_ptr_array_alloc(&pa, 10));
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 110);
+
+    for (idx = alloc1 + 10; idx > alloc1; idx--) {
+        ucs_ptr_array_remove(&pa, idx - 1);
+    }
+
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 100);
+    EXPECT_EQ(alloc1, ucs_ptr_array_alloc(&pa, 10));
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 110);
+
+    for (idx = alloc1; idx < alloc1 + 10; idx++) {
+        ucs_ptr_array_remove(&pa, idx);
+    }
+
+    for (idx = alloc2; idx < alloc2 + 100; idx++) {
+        ucs_ptr_array_remove(&pa, idx);
+    }
+
+    EXPECT_EQ(ucs_ptr_array_is_empty(&pa), 1);
+
+    ucs_ptr_array_cleanup(&pa, 1);
 }
 
 UCS_TEST_F(test_datatype, ptr_array_set_first) {
@@ -602,7 +665,7 @@ UCS_TEST_F(test_datatype, ptr_array_set_first) {
 
     ucs_ptr_array_remove(&pa, 0);
 
-    ucs_ptr_array_cleanup(&pa);
+    ucs_ptr_array_cleanup(&pa, 1);
 }
 
 UCS_TEST_F(test_datatype, ptr_array_random) {
@@ -668,7 +731,7 @@ UCS_TEST_F(test_datatype, ptr_array_random) {
 
     EXPECT_EQ(count_elements, expeced_count);
 
-    ucs_ptr_array_cleanup(&pa);
+    ucs_ptr_array_cleanup(&pa, 1);
 }
 
 UCS_TEST_SKIP_COND_F(test_datatype, ptr_array_perf,
@@ -711,7 +774,7 @@ UCS_TEST_SKIP_COND_F(test_datatype, ptr_array_perf,
 
     ucs_time_t end_time = ucs_get_time();
 
-    ucs_ptr_array_cleanup(&pa);
+    ucs_ptr_array_cleanup(&pa, 1);
 
     double insert_ns = ucs_time_to_nsec(lookup_start_time  - insert_start_time) / count;
     double lookup_ns = ucs_time_to_nsec(foreach_start_time - lookup_start_time) / count;
@@ -769,6 +832,10 @@ UCS_TEST_F(test_datatype, ptr_array_locked_basic) {
 
     ucs_ptr_array_locked_set(&pa, 100, &g);
 
+    index = ucs_ptr_array_locked_alloc(&pa, 200);
+    EXPECT_EQ(301u, pa.super.size);
+    EXPECT_EQ(101u, index);
+
     void *vc;
     int present = ucs_ptr_array_locked_lookup(&pa, 2, &vc);
     ASSERT_TRUE(present);
@@ -790,9 +857,15 @@ UCS_TEST_F(test_datatype, ptr_array_locked_basic) {
     present = ucs_ptr_array_locked_lookup(&pa, 100, &vc);
     EXPECT_EQ(&g, vc);
 
+    present = ucs_ptr_array_locked_lookup(&pa, 101, &vc);
+    EXPECT_EQ(NULL, vc);
+
+    present = ucs_ptr_array_locked_lookup(&pa, 301, &vc);
+    EXPECT_EQ(NULL, vc);
+
     EXPECT_FALSE(ucs_ptr_array_locked_lookup(&pa, 5, &vc));
     EXPECT_FALSE(ucs_ptr_array_locked_lookup(&pa, 99, &vc));
-    EXPECT_FALSE(ucs_ptr_array_locked_lookup(&pa, 101, &vc));
+    EXPECT_FALSE(ucs_ptr_array_locked_lookup(&pa, 302, &vc));
     EXPECT_FALSE(ucs_ptr_array_locked_lookup(&pa, 5005, &vc));
 
     ucs_ptr_array_locked_remove(&pa, 0);
@@ -803,7 +876,7 @@ UCS_TEST_F(test_datatype, ptr_array_locked_basic) {
     ucs_ptr_array_locked_remove(&pa, 6);
     ucs_ptr_array_locked_remove(&pa, 100);
 
-    ucs_ptr_array_locked_cleanup(&pa);
+    ucs_ptr_array_locked_cleanup(&pa, 0);
 }
 
 UCS_TEST_F(test_datatype, ptr_array_locked_random) {
@@ -863,7 +936,7 @@ UCS_TEST_F(test_datatype, ptr_array_locked_random) {
         free(ptr);
     }
 
-    ucs_ptr_array_locked_cleanup(&pa);
+    ucs_ptr_array_locked_cleanup(&pa, 1);
 }
 
 UCS_TEST_SKIP_COND_F(test_datatype, ptr_array_locked_perf,
@@ -901,7 +974,7 @@ UCS_TEST_SKIP_COND_F(test_datatype, ptr_array_locked_perf,
 
     ucs_time_t end_time = ucs_get_time();
 
-    ucs_ptr_array_locked_cleanup(&pa);
+    ucs_ptr_array_locked_cleanup(&pa, 1);
 
     double insert_ns = ucs_time_to_nsec(lookup_start_time  - insert_start_time) / count;
     double lookup_ns = ucs_time_to_nsec(foreach_start_time - lookup_start_time) / count;

openeuler-mirror/kucx

src/ucs/datastruct/ptr_array.c
@@ -13,6 +13,7 @@
 
 #include <ucs/sys/string.h>
 #include <ucs/sys/sys.h>
+#include <ucs/debug/memtrack_int.h>
 #include <ucs/debug/assert.h>
 #include <ucs/debug/log.h>
 
@@ -120,11 +121,11 @@ void ucs_ptr_array_init(ucs_ptr_array_t *ptr_array, const char *name)
     ptr_array->name = name;
 }
 
-void ucs_ptr_array_cleanup(ucs_ptr_array_t *ptr_array)
+void ucs_ptr_array_cleanup(ucs_ptr_array_t *ptr_array, int leak_check)
 {
     unsigned i;
 
-    if (ptr_array->count > 0) {
+    if (leak_check && (ptr_array->count > 0)) {
         ucs_warn("releasing ptr_array with %u used items", ptr_array->count);
         for (i = 0; i < ptr_array->size; ++i) {
             if (!ucs_ptr_array_is_free(ptr_array, i)) {
@@ -175,31 +176,60 @@ static void ucs_ptr_array_grow(ucs_ptr_array_t *ptr_array, unsigned new_size)
     ptr_array->size  = new_size;
 }
 
-unsigned ucs_ptr_array_insert(ucs_ptr_array_t *ptr_array, void *value)
+unsigned
+ucs_ptr_array_bulk_alloc(ucs_ptr_array_t *ptr_array, unsigned element_count)
 {
-    ucs_ptr_array_elem_t *elem;
-    unsigned element_index, new_size;
+    unsigned free_iter, new_size, element_index;
 
-    ucs_assert_always(((uintptr_t)value & UCS_PTR_ARRAY_FLAG_FREE) == 0);
-
-    if (ptr_array->freelist == UCS_PTR_ARRAY_SENTINEL) {
-        new_size = ucs_max(UCS_PTR_ARRAY_INITIAL_SIZE, ptr_array->size * 2);
-        ucs_ptr_array_grow(ptr_array, new_size);
+    if (element_count == 0) {
+        return 0;
     }
 
-    /* Get the first item on the free list */
     element_index = ptr_array->freelist;
-    ucs_assert(element_index != UCS_PTR_ARRAY_SENTINEL);
+    if (element_index == UCS_PTR_ARRAY_SENTINEL) {
+        goto alloc_grow;
+    }
 
-    elem = &ptr_array->start[element_index];
+    free_iter = 1; /* first element from a free-list must be free */
+    do {
+        while ((free_iter < element_count) &&
+               (ucs_ptr_array_is_free(ptr_array, element_index + free_iter))) {
+            free_iter++;
+        }
 
-    /* Remove from free list and populate */
-    ptr_array->freelist = ucs_ptr_array_freelist_get_next(*elem);
-    *elem               = (uintptr_t)value;
+        if (free_iter == element_count) {
+            goto alloc_init;
+        }
 
-    ptr_array->count++;
+        free_iter     = ptr_array->start[element_index];
+        element_index = ucs_ptr_array_freelist_get_next(free_iter);
+        free_iter     = 1;
+    } while (element_index != UCS_PTR_ARRAY_SENTINEL);
+
+alloc_grow:
+    element_index = ptr_array->size;
+    new_size      = ucs_max(2 * ptr_array->size,
+                            ptr_array->size + element_count);
+    ucs_ptr_array_grow(ptr_array, new_size);
+
+alloc_init:
+    for (free_iter = 0; free_iter < element_count; free_iter++) {
+        /* set the value and remove from the free-list */
+        ucs_ptr_array_set(ptr_array, element_index + free_iter, 0);
+    }
 
     return element_index;
+}
+
+unsigned ucs_ptr_array_insert(ucs_ptr_array_t *ptr_array, void *value)
+{
+    unsigned ret = ucs_ptr_array_bulk_alloc(ptr_array, 1);
+
+    ucs_assert(((uintptr_t)value & UCS_PTR_ARRAY_FLAG_FREE) == 0);
+
+    ptr_array->start[ret] = (uintptr_t)value;
+
+    return ret;
 }
 
 void ucs_ptr_array_set(ucs_ptr_array_t *ptr_array, unsigned element_index,
@@ -207,6 +237,8 @@ void ucs_ptr_array_set(ucs_ptr_array_t *ptr_array, unsigned element_index,
 {
     ucs_ptr_array_elem_t *elem;
     unsigned next, free_iter, free_ahead, new_size;
+
+    ucs_assert(((uintptr_t)new_val & UCS_PTR_ARRAY_FLAG_FREE) == 0);
 
     if (ucs_unlikely(element_index >= ptr_array->size)) {
         new_size = ucs_max(ptr_array->size * 2, element_index + 1);
@@ -303,11 +335,12 @@ ucs_ptr_array_locked_init(ucs_ptr_array_locked_t *locked_ptr_array,
     return UCS_OK;
 }
 
-void ucs_ptr_array_locked_cleanup(ucs_ptr_array_locked_t *locked_ptr_array)
+void ucs_ptr_array_locked_cleanup(ucs_ptr_array_locked_t *locked_ptr_array,
+                                  int leak_check)
 {
     ucs_recursive_spin_lock(&locked_ptr_array->lock);
     /* Call unlocked function */
-    ucs_ptr_array_cleanup(&locked_ptr_array->super);
+    ucs_ptr_array_cleanup(&locked_ptr_array->super, leak_check);
     ucs_recursive_spin_unlock(&locked_ptr_array->lock);
 
     /* Destroy spinlock */
@@ -322,6 +355,21 @@ unsigned ucs_ptr_array_locked_insert(ucs_ptr_array_locked_t *locked_ptr_array,
     ucs_recursive_spin_lock(&locked_ptr_array->lock);
     /* Call unlocked function */
     element_index = ucs_ptr_array_insert(&locked_ptr_array->super, value);
+    ucs_recursive_spin_unlock(&locked_ptr_array->lock);
+
+    return element_index;
+}
+
+unsigned
+ucs_ptr_array_locked_bulk_alloc(ucs_ptr_array_locked_t *locked_ptr_array,
+                                unsigned element_count)
+{
+    unsigned element_index;
+
+    ucs_recursive_spin_lock(&locked_ptr_array->lock);
+    /* Call unlocked function */
+    element_index = ucs_ptr_array_bulk_alloc(&locked_ptr_array->super,
+                                             element_count);
     ucs_recursive_spin_unlock(&locked_ptr_array->lock);
 
     return element_index;

test/gtest/ucs/test_datatype.cc
@@ -6,7 +6,9 @@
 */
 
 #include <common/test.h>
+
 extern "C" {
+#include <ucs/debug/memtrack.h>
 #include <ucs/datastruct/array.inl>
 #include <ucs/datastruct/list.h>
 #include <ucs/datastruct/hlist.h>
@@ -542,6 +544,14 @@ UCS_TEST_F(test_datatype, ptr_array_basic) {
 
     ucs_ptr_array_set(&pa, 100, &g);
 
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 7);
+
+    index = ucs_ptr_array_bulk_alloc(&pa, 200);
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 207);
+
+    EXPECT_EQ(301u, pa.size);
+    EXPECT_EQ(101u, index);
+
     void *vc;
     int present = ucs_ptr_array_lookup(&pa, 2, vc);
     ASSERT_TRUE(present);
@@ -563,12 +573,16 @@ UCS_TEST_F(test_datatype, ptr_array_basic) {
     present = ucs_ptr_array_lookup(&pa, 100, vc);
     EXPECT_EQ(&g, vc);
 
+    present = ucs_ptr_array_lookup(&pa, 101, vc);
+    EXPECT_EQ(NULL, vc);
+
+    present = ucs_ptr_array_lookup(&pa, 301, vc);
+    EXPECT_EQ(NULL, vc);
+
     EXPECT_FALSE(ucs_ptr_array_lookup(&pa, 5, vc));
     EXPECT_FALSE(ucs_ptr_array_lookup(&pa, 99, vc));
-    EXPECT_FALSE(ucs_ptr_array_lookup(&pa, 101, vc));
+    EXPECT_FALSE(ucs_ptr_array_lookup(&pa, 302, vc));
     EXPECT_FALSE(ucs_ptr_array_lookup(&pa, 5005, vc));
-
-    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 7);
 
     ucs_ptr_array_remove(&pa, 0);
     ucs_ptr_array_remove(&pa, 1);
@@ -577,15 +591,64 @@ UCS_TEST_F(test_datatype, ptr_array_basic) {
     ucs_ptr_array_remove(&pa, 4);
     ucs_ptr_array_remove(&pa, 6);
 
-    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 1);
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 201);
     EXPECT_EQ(ucs_ptr_array_is_empty(&pa), 0);
 
-    ucs_ptr_array_remove(&pa, 100);
+    for (index = 100; index <= 300; index++) {
+        ucs_ptr_array_remove(&pa, index);
+    }
 
     EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 0);
     EXPECT_EQ(ucs_ptr_array_is_empty(&pa), 1);
 
-    ucs_ptr_array_cleanup(&pa);
+    ucs_ptr_array_cleanup(&pa, 1);
+}
+
+UCS_TEST_F(test_datatype, ptr_array_bulk_alloc) {
+    ucs_ptr_array_t pa;
+    unsigned idx, alloc1, alloc2;
+
+    ucs_ptr_array_init(&pa, "ptr_array alloc test");
+
+    alloc1 = ucs_ptr_array_bulk_alloc(&pa, 10);
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 10);
+
+    EXPECT_GE(pa.size, 10u);
+    EXPECT_EQ(alloc1, 0u);
+
+    alloc2 = ucs_ptr_array_bulk_alloc(&pa, 100);
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 110);
+
+    EXPECT_GE(pa.size, 110u);
+    EXPECT_EQ(alloc2, alloc1 + 10u);
+
+    for (idx = 0; idx < alloc2; idx++) {
+        ucs_ptr_array_remove(&pa, idx);
+    }
+
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 100);
+    EXPECT_EQ(alloc1, ucs_ptr_array_bulk_alloc(&pa, 10));
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 110);
+
+    for (idx = alloc1 + 10; idx > alloc1; idx--) {
+        ucs_ptr_array_remove(&pa, idx - 1);
+    }
+
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 100);
+    EXPECT_EQ(alloc1, ucs_ptr_array_bulk_alloc(&pa, 10));
+    EXPECT_EQ(ucs_ptr_array_get_elem_count(&pa), 110);
+
+    for (idx = alloc1; idx < alloc1 + 10; idx++) {
+        ucs_ptr_array_remove(&pa, idx);
+    }
+
+    for (idx = alloc2; idx < alloc2 + 100; idx++) {
+        ucs_ptr_array_remove(&pa, idx);
+    }
+
+    EXPECT_EQ(ucs_ptr_array_is_empty(&pa), 1);
+
+    ucs_ptr_array_cleanup(&pa, 1);
 }
 
 UCS_TEST_F(test_datatype, ptr_array_set_first) {
@@ -602,7 +665,7 @@ UCS_TEST_F(test_datatype, ptr_array_set_first) {
 
     ucs_ptr_array_remove(&pa, 0);
 
-    ucs_ptr_array_cleanup(&pa);
+    ucs_ptr_array_cleanup(&pa, 1);
 }
 
 UCS_TEST_F(test_datatype, ptr_array_random) {
@@ -668,7 +731,7 @@ UCS_TEST_F(test_datatype, ptr_array_random) {
 
     EXPECT_EQ(count_elements, expeced_count);
 
-    ucs_ptr_array_cleanup(&pa);
+    ucs_ptr_array_cleanup(&pa, 1);
 }
 
 UCS_TEST_SKIP_COND_F(test_datatype, ptr_array_perf,
@@ -711,7 +774,7 @@ UCS_TEST_SKIP_COND_F(test_datatype, ptr_array_perf,
 
     ucs_time_t end_time = ucs_get_time();
 
-    ucs_ptr_array_cleanup(&pa);
+    ucs_ptr_array_cleanup(&pa, 1);
 
     double insert_ns = ucs_time_to_nsec(lookup_start_time  - insert_start_time) / count;
     double lookup_ns = ucs_time_to_nsec(foreach_start_time - lookup_start_time) / count;
@@ -769,6 +832,10 @@ UCS_TEST_F(test_datatype, ptr_array_locked_basic) {
 
     ucs_ptr_array_locked_set(&pa, 100, &g);
 
+    index = ucs_ptr_array_locked_bulk_alloc(&pa, 200);
+    EXPECT_EQ(301u, pa.super.size);
+    EXPECT_EQ(101u, index);
+
     void *vc;
     int present = ucs_ptr_array_locked_lookup(&pa, 2, &vc);
     ASSERT_TRUE(present);
@@ -790,9 +857,15 @@ UCS_TEST_F(test_datatype, ptr_array_locked_basic) {
     present = ucs_ptr_array_locked_lookup(&pa, 100, &vc);
     EXPECT_EQ(&g, vc);
 
+    present = ucs_ptr_array_locked_lookup(&pa, 101, &vc);
+    EXPECT_EQ(NULL, vc);
+
+    present = ucs_ptr_array_locked_lookup(&pa, 301, &vc);
+    EXPECT_EQ(NULL, vc);
+
     EXPECT_FALSE(ucs_ptr_array_locked_lookup(&pa, 5, &vc));
     EXPECT_FALSE(ucs_ptr_array_locked_lookup(&pa, 99, &vc));
-    EXPECT_FALSE(ucs_ptr_array_locked_lookup(&pa, 101, &vc));
+    EXPECT_FALSE(ucs_ptr_array_locked_lookup(&pa, 302, &vc));
     EXPECT_FALSE(ucs_ptr_array_locked_lookup(&pa, 5005, &vc));
 
     ucs_ptr_array_locked_remove(&pa, 0);
@@ -803,7 +876,7 @@ UCS_TEST_F(test_datatype, ptr_array_locked_basic) {
     ucs_ptr_array_locked_remove(&pa, 6);
     ucs_ptr_array_locked_remove(&pa, 100);
 
-    ucs_ptr_array_locked_cleanup(&pa);
+    ucs_ptr_array_locked_cleanup(&pa, 0);
 }
 
 UCS_TEST_F(test_datatype, ptr_array_locked_random) {
@@ -863,7 +936,7 @@ UCS_TEST_F(test_datatype, ptr_array_locked_random) {
         free(ptr);
     }
 
-    ucs_ptr_array_locked_cleanup(&pa);
+    ucs_ptr_array_locked_cleanup(&pa, 1);
 }
 
 UCS_TEST_SKIP_COND_F(test_datatype, ptr_array_locked_perf,
@@ -901,7 +974,7 @@ UCS_TEST_SKIP_COND_F(test_datatype, ptr_array_locked_perf,
 
     ucs_time_t end_time = ucs_get_time();
 
-    ucs_ptr_array_locked_cleanup(&pa);
+    ucs_ptr_array_locked_cleanup(&pa, 1);
 
     double insert_ns = ucs_time_to_nsec(lookup_start_time  - insert_start_time) / count;
     double lookup_ns = ucs_time_to_nsec(foreach_start_time - lookup_start_time) / count;